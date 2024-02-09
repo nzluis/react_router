b7c03392d5521d14a545e398f58411c76246998c
luis <luisnavarroFS@gmail.com>
2024-02-09 20:58:49 +0100

    set cBR & cRE & add routes

--- src/app/App.js
+++ src/app/App.js
@@ -10,16 +10,20 @@ import Profile from "../components/Profile";
 import EditProfileForm from "../components/EditProfileForm";
 import Root from "../components/Root";
 
-import { Route, RouterProvider } from 'react-router-dom';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 
 import "./App.css";
 
-const router = 
+const router = createBrowserRouter( createRoutesFromElements(
   /* Wrap this Root Route to create Router here */
   <Route path="/" element={ <Root/> }>
-    {/* Add Routes here! */}
+    <Route path="about" element={<About/>} />
+    <Route path="sign-up" element={<SignUp/>} />
+    <Route path="articles" element={<Articles/>} />
+    <Route path="categories" element={<Categories/>} />
+    <Route path="profile" element={<Profile/>} />
   </Route>
-
+))
 function App() {
   return (
     <>
