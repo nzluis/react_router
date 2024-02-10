0940b41691cf1e51ff29142d883a5bf65732bbc5
luis <luisnavarroFS@gmail.com>
2024-02-10 12:42:52 +0100

    nested routes on Profile&Categories

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import { selectCurrentUser, selectIsLoggedIn } from "../features/session/sessionSlice";
 
 export default function Profile () {
@@ -14,6 +14,7 @@ export default function Profile () {
       <h1>{currentUser.username}</h1>
       <Link to={`edit`}>Edit</Link>
       {/* Tell React Router where to render child routes` */}
+      <Outlet/>
     </main>
   )
 }
